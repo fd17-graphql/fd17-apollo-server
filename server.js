c13a6657450c2454f9b3c679af595c8c50704271
Philipp Schneider <github@phil-schneider.de>
2017-06-08 13:12:44 +0000

    Cors aktiviert für Options requests

--- server.js
+++ server.js
@@ -5,13 +5,34 @@ import Resolvers from './data/fd17-resolvers';
 
 const GRAPHQL_PORT = process.env.PORT || 8080;
 
+const cors = require("cors");
 const graphQLServer = express();
+
+graphQLServer.use('/graphql', cors())
+
+
+var apolloServerNew =  apolloServer({
+  graphiql: true,
+  pretty: true,
+  schema: Schema,
+  //mocks: Mocks,
+  resolvers: Resolvers,
+})
+
+
+graphQLServer.get('/graphql', apolloServerNew);
+graphQLServer.post('/graphql', apolloServerNew);
+
+/**
+
 graphQLServer.use('/graphql', apolloServer({
   graphiql: true,
   pretty: true,
   schema: Schema,
   resolvers: Resolvers,
 }));
+**/
+
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`
 ));
