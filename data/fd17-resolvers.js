aa48ea99ed1fd83b00c1fcc6d8203ad98f6ce1a1
PZ <pfust75@gmail.com>
2017-06-08 19:18:40 +0200

    blah

--- data/fd17-resolvers.js
+++ data/fd17-resolvers.js
@@ -32,7 +32,7 @@ const resolvers = {
       });
     }
   },
-
+//blah
   Partner: {
     contracts(partner) {
       return ContractModelMongoose.find({ 'fk_partnerNumber': partner.partnerNumber }).exec().then((contract) => {
